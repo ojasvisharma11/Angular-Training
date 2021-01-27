f75ae99429b33a7f5e6c3b4dffb52ced1e6d2fd3
ojasvisharma11 <77456127+ojasvisharma11@users.noreply.github.com>
2021-01-27 11:02:40 +0530

    Update User.ts

--- Day 09/Bank Accounts/User.ts
+++ Day 09/Bank Accounts/User.ts
@@ -1,7 +1,7 @@
 class User extends Citizen{
     user_id: number;
-    constructor(user_id: number){
-        super()
+    constructor(name: string, dob: object, gender: string, phone_no: number, mail: string, houseno:number, street:string, area:string, zip:number, city:string, state:string, country:string, adhaarid:number, passportid: string, voterid: string, pancard: string, user_id: number){
+        super(name, dob, gender, phone_no, mail, houseno, street, area, zip, city, state, country, adhaarid, passportid, voterid, pancard)
         this.user_id = user_id
     }
     display(){
